ffbccfe31174d13ca79d8cbde55952a28958d344
JonathanAlfah <jonalfaro1605@gmail.com>
2026-01-06 16:42:23 -0600

    initial structure

--- app/page.tsx
+++ app/page.tsx
@@ -1,64 +1,85 @@
-import Image from "next/image";
+
 
 export default function Home() {
   return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.tsx file.
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
-        </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
-            />
-            Deploy Now
+    <div>
+      <nav className="fixed top-4  bg-white/50 w-[calc(100%-2rem)] max-w-6xl h-14 left-1/2 -translate-x-1/2 rounded-full shadow-md">
+         <div className="flex h-16 items-center px-24 justify-between">
+          <a className=" font-semibold text-gray-900" href="#">
+            ALFAHER
           </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
+
+          <ul className="items-center gap-4 flex">
+            <li>
+              <a
+                href="#inicio"
+                className="text-sm text-gray-700 hover:text-gray-900"
+              >
+                Inicio
+              </a>
+            </li>
+            <li>
+              <a
+                href="#proyectos"
+                className="text-sm text-gray-700 hover:text-gray-900"
+              >
+                Proyectos
+              </a>
+            </li>
+            <li>
+              <a
+                href="#experiencia"
+                className="text-sm text-gray-700 hover:text-gray-900"
+              >
+                Experiencia
+              </a>
+            </li>
+            <li>
+              <a
+                href="#sobremi"
+                className="text-sm text-gray-700 hover:text-gray-900"
+              >
+                Sobre mi
+              </a>
+            </li>
+            <li>
+              <a
+                href="#contacto"
+                className="text-sm text-gray-700 hover:text-gray-900"
+              >
+                Contacto
+              </a>
+            </li>
+          </ul>
+
+          <div>
+            <button
+              type="button"
+              className="h-10 w-10 items-center justify-center rounded-xl text-gray-800 cursor-pointer"
+            >
+              <i className="text-semibold ">ES</i>
+            </button>
+            <button
+              type="button"
+              className="h-10 w-10 items-center justify-center rounded-xl text-gray-800 cursor-pointer"
+            >
+              <i className="bi bi-brightness-high-fill text-xl"></i>
+            </button>
+          </div>
         </div>
+      </nav>
+
+      <main className="scroll-smooth px-50 bg-gradient-to-tr from-gray-[#0D0D0D] to-[#0D004A]">
+        <section id="inicio" className=" w-full h-screen flex flex-col items-center justify-center">
+        </section>
+        <section id="proyectos" className=" w-full h-screen flex flex-col items-center justify-center">
+        </section>
+        <section id="experiencia" className=" w-full h-screen flex flex-col items-center justify-center">
+        </section>
+        <section id="sobremi" className=" w-full h-screen flex flex-col items-center justify-center">
+        </section>
+        <section id="contacto" className=" w-full h-screen flex flex-col items-center justify-center">
+        </section>
       </main>
     </div>
   );
